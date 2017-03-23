d494121489948d1b72699a9299d9c3576fb96b28
ladetryne <ladetryne@gmail.com>
2017-03-23 10:59:00 +0100

    Del 12 complete

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,19 @@
 |
 */
 
+
+
 Route::get('/contact', 'PagesController@getContact'); 
-
 Route::get('/about', 'PagesController@getAbout'); 
-
 Route::get('/', 'PagesController@getIndex');
+
+Route::resource('posts', 'PostController'); //index, create, store, show, edit, update, destroy
+//  GET|HEAD  | posts             | posts.index   | App\Http\Controllers\PostController@index  
+//  POST      | posts             | posts.store   | App\Http\Controllers\PostController@store  
+//  GET|HEAD  | posts/create      | posts.create  | App\Http\Controllers\PostController@create 
+//  GET|HEAD  | posts/{post}      | posts.show    | App\Http\Controllers\PostController@show   
+//  PUT|PATCH | posts/{post}      | posts.update  | App\Http\Controllers\PostController@update 
+//  DELETE    | posts/{post}      | posts.destroy | App\Http\Controllers\PostController@destroy
+//  GET|HEAD  | posts/{post}/edit | posts.edit    | App\Http\Controllers\PostController@edit   
+// -----------+-------------------+---------------+--------------------------------------------
 
